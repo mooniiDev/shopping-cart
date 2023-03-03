edabfbbf0ed3b300535c7afa25b5cc386ee27926
mooniiDev <mooniidev@proton.me>
2023-03-03 19:42:04 +0200

    Write unit tests to check if Header renders correct child components on different views

--- src/__tests__/header/Header.test.js
+++ src/__tests__/header/Header.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import Header from '../../components/Header/Header';
 import useScreenSize from '../../utils/useScreenSize';
 
@@ -11,20 +11,50 @@ describe('Header', () => {
       useScreenSize.mockImplementation(() => true);
     });
 
-    it('renders correctly', () => {
+    // Test if a view on mobile matches a snapshot
+    it('matches snapshot', () => {
       const tree = render(<Header />).container;
       expect(tree).toMatchSnapshot('mobile');
     });
+
+    // Test if Header renders correct child components
+    it('renders toggle button, logo, checkout button and navigation items', () => {
+      render(<Header />);
+
+      const toggleButton = screen.getByTestId('toggle-button');
+      const logo = screen.getByTestId('logo');
+      const navigation = screen.getByTestId('navigation');
+      const checkoutButton = screen.getByTestId('checkout-button');
+
+      expect(toggleButton).toBeInTheDocument();
+      expect(logo).toBeInTheDocument();
+      expect(navigation).toBeInTheDocument();
+      expect(checkoutButton).toBeInTheDocument();
+    });
   });
 
+  // Test if a view on desktop matches a snapshot
   describe('on desktop', () => {
     beforeEach(() => {
       useScreenSize.mockImplementation(() => false);
     });
 
-    it('renders correctly', () => {
+    it('matches snapshot', () => {
       const tree = render(<Header />).container;
       expect(tree).toMatchSnapshot('desktop');
+    });
+
+    // Test if Header renders correct child components
+    it('renders logo, navigation items and checkout button', () => {
+      render(<Header />);
+
+      const logo = screen.getByTestId('logo');
+      const navigation = screen.getByTestId('navigation');
+      const checkoutButton = screen.getByTestId('checkout-button');
+
+      expect(logo).toBeInTheDocument();
+      expect(navigation).toBeInTheDocument();
+      expect(checkoutButton).toBeInTheDocument();
     });
   });
 });
